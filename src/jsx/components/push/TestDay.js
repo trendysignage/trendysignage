8cb686c9c89d910dd0c32afc16dc6bda00165f49
akali89 <maheep@dev-story.com>
2023-06-25 10:26:36 +0530

    day shedule

--- src/jsx/components/push/TestDay.js
+++ src/jsx/components/push/TestDay.js
@@ -5,7 +5,12 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import defaultComparisonIcon from "../../../img/default-comparison-icon.png";
-import { BASE_URL, getAllComposition, getAllMedia } from "../../../utils/api";
+import {
+  BASE_URL,
+  getAllComposition,
+  getAllMedia,
+  saveSequence,
+} from "../../../utils/api";
 import {
   getDatetimeIn12Hours,
   humanReadableFormattedDateString,
@@ -25,26 +30,35 @@ const MyCustomPlugin = createPlugin({
 });
 
 export default function TestDay() {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState({});
+  const [sequence, setSequence] = useState([]);
+  console.log(sequence, "top");
+  //   const [mergedArray, setMergedArray] = useState(events);
+  //   console.log(mergedArray, "mergedArray");
   const [eventsId, setEventsId] = useState("");
-  //   console.log(events, "eventsevents");
+  const [renderTime, setRenderTime] = useState("");
+  console.log(events, "eventsevents");
+  //   console.log(resizeEvents, "resizeEvents");
   //   console.log(events, "events events");
   const history = useHistory();
-  const { id } = useParams();
-  //   console.log(id, "testday id");
+  const { id, schedulename } = useParams();
 
   const { data: allComposition, mutate } = useSWR(
     "/vendor/layouts/compositions",
     getAllComposition
   );
-
+  let timeFormet = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
   // load external events
   useEffect(() => {
     let draggableEl = document.getElementById("external-events");
     new Draggable(draggableEl, {
       itemSelector: ".fc-event",
       eventData: function (eventEl) {
-        // console.log(eventEl, "eventEl eventEl");
+        console.log(eventEl, "eventEl eventEl");
         let id = eventEl.dataset.id;
         let sourceId = eventEl.getAttribute("sourceId");
         let title = eventEl.getAttribute("title");
@@ -62,55 +76,141 @@ export default function TestDay() {
       },
     });
   }, []);
+  function eventFunction(info) {
+    console.log(info, "info inside eventFunction");
+
+    const newArray = events;
+    const id = info.el.fcSeg.eventRange.def.sourceId;
+    const newEvent = {
+      id,
+      timing: info.el.innerText.split("\n\n")[1],
+    };
+    newArray[id] = newEvent;
+    setEvents(newArray);
+    console.log(events, "newEvent newEvent");
+  }
 
   // handle event receive
   const handleEventReceive = (eventInfo) => {
-    // console.log(eventInfo, "handleEventReceivehandleEventReceive");
-    const { id } = eventInfo.draggedEl.dataset;
-    // const timing = eventInfo.draggedEl.getAttribute("data-timing");
-    // const timeText = eventInfo.timeText; // Store eventInfo.timeText in a variable
+    console.log(eventInfo, "handleEventReceivehandleEventReceive");
+    const id = eventInfo.event._def.sourceId;
+    const time =
+      renderTime +
+      " - " +
+      "0" +
+      (Number(renderTime.split(":")[0]) + 1 + ":" + renderTime.split(":")[1]);
     const newEvent = {
       id: id,
-      //   timing: timing,
-
-      //   timeText: timeText, // Include the timeText property
+      timing: time,
     };
-    setEvents([...events, newEvent]);
+    setEvents({ ...events, [id]: newEvent });
   };
   // handle deletion of an event
 
   const handleEventClick = (info) => {
-    // console.log(info, "sssss");
+    console.log(info, "sssss");
+
+    const compositionToDelete = info.event._def.sourceId;
+    const deleteTiming = events;
+    delete deleteTiming[compositionToDelete];
+    setEvents(deleteTiming);
+    // console.log(compositionToDelete, "compositionToDelete id");
+    // sequence.timings = sequence?.timings?.filter(
+    //   (timing) => timing.composition !== compositionToDelete
+    // );
+    console.log(events, "inside delete");
+
+    // console.log(sequence, "when remove");
+    // setSequence(sequence);
     info.event.remove();
   };
-  function reciveTime(value) {
-    // console.log(value);
-    return value;
+  async function handleSubmit() {
+    const scheduleId = id;
+    const name = schedulename;
+    const timing = [];
+
+    Object.keys(events).forEach(function (key) {
+      timing.push({
+        composition: events[key].id,
+        startTime:
+          new Date().toISOString().slice(0, 10) +
+          "T" +
+          events[key].timing.split(" - ")[0] +
+          ":00Z",
+        endTime:
+          new Date().toISOString().slice(0, 10) +
+          "T" +
+          events[key].timing.split(" - ")[1] +
+          ":00Z",
+      });
+    });
+
+    // events.forEach((item, i) =>
+    //   timing.push({
+    //     composition: item.id,
+    //     startTime:
+    //       new Date().toISOString().slice(0, 10) +
+    //       "T" +
+    //       item.timing.split(" - ")[0] +
+    //       ":00Z",
+    //     endTime:
+    //       new Date().toISOString().slice(0, 10) +
+    //       "T" +
+    //       item.timing.split(" - ")[1] +
+    //       ":00Z",
+    //   })
+    // );
+    // console.log(first)
+    // const timings = Object.values(events)?.map((composition) => {
+    //   const startTime =
+    //     new Date().toISOString().slice(0, 10) +
+    //     "T" +
+    //     composition.timing.split(" - ")[0] +
+    //     ":00Z";
+    //   const endTime =
+    //     new Date().toISOString().slice(0, 10) +
+    //     "T" +
+    //     composition.timing.split(" - ")[1] +
+    //     ":00Z";
+
+    //   return {
+    //     composition: composition.id,
+    //     startTime: startTime,
+    //     endTime: endTime,
+    //   };
+    // });
+
+    const outputObject = {
+      scheduleId: scheduleId,
+      name: name,
+      timings: timing,
+    };
+
+    console.log(outputObject, "yyyy");
+    // setSequence(outputObject);
+    // console.log(sequence, "outputObject");
+    // e.preventDefault();
+
+    // await saveSequence(sequence).then((res) => {
+    //   console.log(res, "res save schedule");
+    //   //   if (res.data.statusCode === 200) {
+    //   //     console.log(res.data.data.name);
+    //   //     history.push(`/testday/${res.data.data._id}/${res.data.data.name}`);
+    //   //   }
+    // });
   }
   function moveToMonth() {
-    // history.push("/design-month-schedule");
-    const element = document.getElementsByClassName("fc-event-main");
-    console.log(element, "element element");
+    history.push("/design-month-schedule");
   }
   function renderEventContent(eventInfo) {
-    console.log(eventInfo, "eventInfo inside renderEventContent");
     const { event } = eventInfo;
     const { title } = event;
-    reciveTime(eventInfo.timeText);
-    console.log(
-      eventInfo.event.sourceId,
-      "eventInfo.sourceId eventInfo.sourceId"
-    );
-    // const newEvent = {
-    //   id: eventInfo.sourceId,
-    //   timing: eventInfo.timeText,
 
-    //   //   timeText: timeText, // Include the timeText property
-    // };
-    // setEvents([...events, newEvent]);
+    setRenderTime(eventInfo.timeText);
+
     return (
       <>
-        <div className={`fullcalendar-main-container ${eventInfo.sourceId}`}>
+        <div className={`fullcalendar-main-container`}>
           <img
             src={event.extendedProps.custom}
             className="day-schedule-fullcalendar-img"
@@ -132,15 +232,18 @@ export default function TestDay() {
   //   console.log(allComposition, "allComposition allComposition");
   return (
     <div className="App">
-      <div className="d-flex justify-content-end">
-        <Button
-          className="mr-2"
-          variant="info add-screen-btn"
-          onClick={() => moveToMonth()}
-        >
-          Save Sequence
-        </Button>
-      </div>
+      {renderTime && (
+        <div className="d-flex justify-content-end">
+          <Button
+            className="mr-2"
+            variant="info add-screen-btn"
+            onClick={() => handleSubmit()}
+          >
+            Save Sequence
+          </Button>
+        </div>
+      )}
+
       <div>
         <div style={{ float: "left", width: "50%", height: "100vh" }}>
           <Table
@@ -256,6 +359,7 @@ export default function TestDay() {
             slotDuration="00:10:00"
             slotLabelInterval={{ hours: 1 }}
             allDaySlot={false}
+            eventTimeFormat={timeFormet}
             editable={true}
             selectable={false}
             selectMirror={true}
@@ -267,6 +371,7 @@ export default function TestDay() {
             eventContent={renderEventContent}
             contentHeight="700px"
             events={events}
+            eventResize={eventFunction}
           ></FullCalendar>
         </div>
       </div>
