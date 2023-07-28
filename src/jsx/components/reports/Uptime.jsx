f151782f0ac3150151bc48abbfbd9360b59ee2a0
cool_anuj <cool_anuj@bitbucket.org>
2023-07-28 16:14:12 +0530

    setting

--- src/jsx/components/reports/Uptime.jsx
+++ src/jsx/components/reports/Uptime.jsx
@@ -4,6 +4,32 @@ import Datatable from "react-data-table-component";
 
 export default function Uptime({reportData}) {
 
+    const getUptimeData = (data) => {
+        const sumOfTime = data?.uptimeReport?.reduce(
+            (total, obj) => total + obj?.time,
+            0
+          );
+
+          // Convert the sum to hours
+          const sumInHours = sumOfTime;
+          const hours = Math.floor(sumOfTime / 60);
+          const minutes = sumOfTime % 60;
+
+          const formattedSum = `${hours} hr ${
+            minutes < 10 ? "0" : ""
+          }${Math.floor(minutes)} min`;
+
+          const average = sumInHours / data?.uptimeReport?.length;
+          const aveHours = Math.floor(average / 60);
+          const aveMinutes = average % 60;
+
+          const aveFormattedSum = `${aveHours} hr ${
+            aveMinutes < 10 ? "0" : ""
+          }${Math.floor(aveMinutes)} min`;
+
+          return [formattedSum, aveFormattedSum];
+    }
+
     const columns = [
         {
             name : "Screen",
@@ -11,18 +37,18 @@ export default function Uptime({reportData}) {
         },
         {
             name : "Total Uptime",
-            selector : (row) => row.loop
+            selector : (row) => getUptimeData(row)[0]
         },
         {
             name : "Total Avg Time",
-            selector : (row) => row.duration
+            selector : (row) => getUptimeData(row)[1]
         },
     ];
 
 
   return (
     <>
-      <Table
+      {/* <Table
         responsive
         className="custom-table screen-table"
         style={{ height: "100%" }}
@@ -39,28 +65,7 @@ export default function Uptime({reportData}) {
         <tbody>
           {reportData.length > 0 &&
             reportData.map((data) => {
-              const sumOfTime = data?.uptimeReport?.reduce(
-                (total, obj) => total + obj?.time,
-                0
-              );
-
-              // Convert the sum to hours
-              const sumInHours = sumOfTime;
-              const hours = Math.floor(sumOfTime / 60);
-              const minutes = sumOfTime % 60;
-
-              const formattedSum = `${hours} hr ${
-                minutes < 10 ? "0" : ""
-              }${Math.floor(minutes)} min`;
-
-              const average = sumInHours / data?.uptimeReport?.length;
-              const aveHours = Math.floor(average / 60);
-              const aveMinutes = average % 60;
-
-              const aveFormattedSum = `${aveHours} hr ${
-                aveMinutes < 10 ? "0" : ""
-              }${Math.floor(aveMinutes)} min`;
-
+              
               return (
                 <tr key={data?._id}>
                   <td>{data?.name}</td>
@@ -71,7 +76,8 @@ export default function Uptime({reportData}) {
             })}
         </tbody>
         {reportData?.length === 0 && <h3 className="mt-5">No Report Found</h3>}
-      </Table>
+      </Table> */}
+      <Datatable columns={columns} data={reportData} pagination />
     </>
   );
 }
