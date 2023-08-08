93c94d6b9d6f7b8aaa17444847676ba01443dec2
akali89 <maheep@dev-story.com>
2023-08-08 19:07:52 +0530

    update

--- src/jsx/components/settings/profile.jsx
+++ src/jsx/components/settings/profile.jsx
@@ -4,142 +4,147 @@ import menuIcon from "../../../img/menu-icon.png";
 import assignIcon from "../../../img/assign-icon.png";
 import takeScreenshotIcon from "../../../img/tack-screenshot-icon.png";
 import { addDeviceProfile, deleteDeviceProfile } from "../../../utils/api";
-import {
-    humanReadableFormattedDateString,
-  } from "../../../utils/UtilsService";
+import { humanReadableFormattedDateString } from "../../../utils/UtilsService";
 import { toast } from "react-toastify";
-import AddDeviceProfile from '../../modals/AddDeviceProfile';
+import AddDeviceProfile from "../../modals/AddDeviceProfile";
 import SelectScreenModal from "../../modals/SelectScreenModal";
 import Datatable from "react-data-table-component";
 
-
 const Profile = ({ allDeviceProfile, setIsRefresh, isRefresh, loading }) => {
+  const [showProfileModel, setShowProfileModel] = useState(false);
+  const [profileData, setProfileData] = useState(null);
+  const [showPublishPopUp, setShowPublishPopUp] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [selectedScreen, setSelectedScreen] = useState([]);
 
-const [showProfileModel, setShowProfileModel] = useState(false);
-const [profileData, setProfileData] = useState(null);
-const [showPublishPopUp, setShowPublishPopUp] = useState(false);
-const [selected, setSelected] = useState(null);
-const [selectedScreen, setSelectedScreen] = useState([]);
+  const handleEditiUser = (e, item) => {
+    e.preventDefault();
+    setShowProfileModel(true);
+    setProfileData(item);
+    console.log("Update Profile");
+  };
 
-
-const handleEditiUser = (e, item) => {
-  e.preventDefault();
-  setShowProfileModel(true);
-  setProfileData(item)
-  console.log("Update Profile")
-}
-
-const handleAssignScreen = (e, id, item) => {
+  const handleAssignScreen = (e, id, item) => {
     e.preventDefault();
     setShowPublishPopUp(true);
-    setSelected(id)
-    setSelectedScreen(item.screens)
-    console.log("Assign Screen")
-  }
+    setSelected(id);
+    setSelectedScreen(item.screens);
+    console.log("Assign Screen");
+  };
 
-const handleDelete = async(e, id) => {
+  const handleDelete = async (e, id) => {
     e.preventDefault();
     await deleteDeviceProfile(id);
     toast.success("Profile has been deleted successfully !!!", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
     });
     setIsRefresh(!isRefresh);
-}
+  };
 
-const renderAction = (value) => {
-  return <Dropdown className="dropdown-toggle">
-            <Dropdown.Toggle variant="" className="p-0  mb-2">
-              <span className="table-menu-icon">
+  const renderAction = (value) => {
+    return (
+      <Dropdown className="dropdown-toggle profile-dropdown">
+        <Dropdown.Toggle variant="" className="p-0  mb-2">
+          <span className="table-menu-icon">
+            <img
+              className="menu-img img-fluid"
+              src={menuIcon}
+              alt="menu-icon"
+            />
+          </span>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={(e) => {
+              handleEditiUser(e, value);
+            }}
+            className="dropdown-list-item"
+          >
+            <div className="d-flex">
+              <div className="dropdown-list-icon">
                 <img
-                  className="menu-img img-fluid"
-                  src={menuIcon}
+                  className="dropdown-list-img img-fluid"
+                  src={assignIcon}
                   alt="menu-icon"
                 />
-              </span>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              
-              <Dropdown.Item 
-              onClick={(e) => {handleEditiUser(e, value)}}
-                className="dropdown-list-item">
-                <div className="d-flex">
-                  <div className="dropdown-list-icon">
-                    <img
-                      className="dropdown-list-img img-fluid"
-                      src={assignIcon}
-                      alt="menu-icon"
-                    />
-                  </div>
-                  <div className="dropdown-menu-list">
-                    <span className="menu-heading">Edit</span>
-                  </div>
-                </div>
-              </Dropdown.Item>
-              <Dropdown.Item 
-                onClick={(e)=>{handleAssignScreen(e, value._id, value)}}
-                className="dropdown-list-item">
-                <div className="d-flex">
-                  <div className="dropdown-list-icon">
-                    <img
-                      className="dropdown-list-img img-fluid"
-                      src={takeScreenshotIcon}
-                      alt="menu-icon"
-                    />
-                  </div>
-                  <div className="dropdown-menu-list">
-                    <span className="menu-heading">Assigned Screen</span>
-                  </div>
-                </div>
-              </Dropdown.Item>
-              <Dropdown.Item 
-                onClick={(e)=>{handleDelete(e, value._id)}}
-                className="dropdown-list-item">
-                <div className="d-flex">
-                  <div className="dropdown-list-icon">
-                    <img
-                      className="dropdown-list-img img-fluid"
-                      src={takeScreenshotIcon}
-                      alt="menu-icon"
-                    />
-                  </div>
-                  <div className="dropdown-menu-list">
-                    <span className="menu-heading">Delete</span>
-                  </div>
-                </div>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>;
-}
+              </div>
+              <div className="dropdown-menu-list">
+                <span className="menu-heading">Edit</span>
+              </div>
+            </div>
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={(e) => {
+              handleAssignScreen(e, value._id, value);
+            }}
+            className="dropdown-list-item"
+          >
+            <div className="d-flex">
+              <div className="dropdown-list-icon">
+                <img
+                  className="dropdown-list-img img-fluid"
+                  src={takeScreenshotIcon}
+                  alt="menu-icon"
+                />
+              </div>
+              <div className="dropdown-menu-list">
+                <span className="menu-heading">Assigned Screen</span>
+              </div>
+            </div>
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={(e) => {
+              handleDelete(e, value._id);
+            }}
+            className="dropdown-list-item"
+          >
+            <div className="d-flex">
+              <div className="dropdown-list-icon">
+                <img
+                  className="dropdown-list-img img-fluid"
+                  src={takeScreenshotIcon}
+                  alt="menu-icon"
+                />
+              </div>
+              <div className="dropdown-menu-list">
+                <span className="menu-heading">Delete</span>
+              </div>
+            </div>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  };
 
-const columns = [
-  {
-      name : "Name",
-      selector : (row) => row?.name,
+  const columns = [
+    {
+      name: "Name",
+      selector: (row) => row?.name,
       sortable: true,
-  },
-  {
-      name : "Created",
-      selector : (row) => humanReadableFormattedDateString(row.createdAt),
+    },
+    {
+      name: "Created",
+      selector: (row) => humanReadableFormattedDateString(row.createdAt),
       sortable: true,
-  },
-  {
-      name : "Updated",
-      selector : (row) => humanReadableFormattedDateString(row.createdAt),
+    },
+    {
+      name: "Updated",
+      selector: (row) => humanReadableFormattedDateString(row.createdAt),
       sortable: true,
-  },
-  {
-    name : " ",
-    selector : (row) => renderAction(row),
-    sortable: false,
-},
-];
+    },
+    {
+      name: " ",
+      selector: (row) => renderAction(row),
+      sortable: false,
+    },
+  ];
   // const rows = [];
   // if(allDeviceProfile){
   //   allDeviceProfile.forEach((item) => {
@@ -151,13 +156,12 @@ const columns = [
   //             assignedScreen:item.screens.length,
   //             action:item
   //         });
-  //     });     
+  //     });
   // }
-
 
   return (
     <>
-     <AddDeviceProfile
+      <AddDeviceProfile
         open={showProfileModel}
         setShowProfileModel={setShowProfileModel}
         setIsRefresh={setIsRefresh}
@@ -165,14 +169,21 @@ const columns = [
         setProfileData={setProfileData}
         type="edit"
       />
-      <SelectScreenModal 
+      <SelectScreenModal
         showPublishPopUp={showPublishPopUp}
         setShowPublishPopUp={setShowPublishPopUp}
         selected={selected}
         setIsRefresh={setIsRefresh}
-        selectedScreen={selectedScreen} setSelectedScreen={setSelectedScreen}
+        selectedScreen={selectedScreen}
+        setSelectedScreen={setSelectedScreen}
       />
-      <Datatable columns={columns} data={allDeviceProfile} pagination sorting />
+      <Datatable
+        className="profile"
+        columns={columns}
+        data={allDeviceProfile}
+        pagination
+        sorting
+      />
       {/* <Table responsive className="custom-table screen-table">
         <thead>
           <tr>
