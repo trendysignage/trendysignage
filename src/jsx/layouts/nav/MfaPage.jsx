19d1ef3e056de08d50c1fe59887e94d432266793
akali89 <maheep@dev-story.com>
2023-09-21 18:52:43 +0530

    verification

--- src/jsx/layouts/nav/MfaPage.jsx
+++ src/jsx/layouts/nav/MfaPage.jsx
@@ -1,40 +1,43 @@
-import React, {useState} from 'react';
-import {connect, useDispatch } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import { logout } from '../../../store/actions/AuthActions';
-import { isAuthenticated } from '../../../store/selectors/AuthSelectors';
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import { logout } from "../../../store/actions/AuthActions";
+import { isAuthenticated } from "../../../store/selectors/AuthSelectors";
 import Switch from "react-switch";
-import { verification2FaAuth } from '../../../store/actions/AuthActions';
-import { mfaEnablePost } from '../../../utils/api';
+import { verification2FaAuth } from "../../../store/actions/AuthActions";
+import { mfaEnablePost } from "../../../utils/api";
 
-
-function MfaPage(props){
-    const dispatch = useDispatch();
-    const [mfaEnabled, setMfaEnabled] = useState(false);
-    const handleMfa = async (e) => {
-        setMfaEnabled(!mfaEnabled);
-        //makeApi for MFA
-        //await mfaEnablePost(mfaEnabled);
-        dispatch(verification2FaAuth(!mfaEnabled, props.history));
-    }
-    return(
-        <div className="align-items-center justify-content-between">
-            <label className="mb-0 mr-3">MFA Enabled</label>
-            <Switch
-                onColor="#B3005E"
-                onChange={(e) => {handleMfa(e)}}
-                checked={mfaEnabled}
-                name="deviceTime"
-                id="deviceTime"
-                className="react-switch"
-            />
-        </div>
-    )
-} 
+function MfaPage(props) {
+  const dispatch = useDispatch();
+  const [mfaEnabled, setMfaEnabled] = useState(false);
+  const handleMfa = async (e) => {
+    setMfaEnabled(!mfaEnabled);
+    //makeApi for MFA
+    //await mfaEnablePost(mfaEnabled);
+    dispatch(verification2FaAuth(!mfaEnabled, props.history));
+  };
+  return (
+    <div className="align-items-center justify-content-between">
+      <div className="d-flex align-items-center mb-4 ml-4">
+        <label className="mb-0 mr-3 text-white">MFA Enabled</label>
+        <Switch
+          onColor="#B3005E"
+          onChange={(e) => {
+            handleMfa(e);
+          }}
+          checked={mfaEnabled}
+          name="deviceTime"
+          id="deviceTime"
+          className="react-switch"
+        />
+      </div>
+    </div>
+  );
+}
 const mapStateToProps = (state) => {
-    return {
-        isAuthenticated: isAuthenticated(state),
-    };
+  return {
+    isAuthenticated: isAuthenticated(state),
+  };
 };
 
 export default withRouter(connect(mapStateToProps)(MfaPage));
